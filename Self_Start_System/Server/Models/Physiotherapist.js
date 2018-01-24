1331ace60749a2177a396fdfa585adc6ad21faed
ahart45 <ahart45@uwo.ca>
2018-01-24 17:50:34 -0500

    Add Physiotherapist schema

---
+++ Self_Start_System/Server/Models/Physiotherapist.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var physiotherapistSchema = mongoose.Schema(
+	{
+		familyName: String,
+		givenName: String,
+		email: String,
+		dateHired: Date,
+		dateFinished: Date
+		treatments: [{type: mongoose.Schema.ObjectId, ref: 'Treatments'}]
+	}
+);
+
+var Physiotherapist = mongoose.model('Physiotherapist', physiotherapistSchema);
+exports.Model = Physiotherapist;
