52c235b5b437d6a17a1af42b2123adebe7dac5ab
Leo Zhang <leozhang@dyn129-100-133-211.eduroam.uwo.ca>
2018-01-24 17:14:05 -0500

    added post and put to recommendation route

--- Self_Start_System/Server/Routes/Recommendation.js
+++ Self_Start_System/Server/Routes/Recommendation.js
@@ -2,6 +2,7 @@ const Recommendation = require('../models/recommendation');
 
 module.exports = function (router){
 
+    //get route
     router.get('/recommendation/:recommendationID', function (req, res) {
         if (!(req.params.recommendationID)) {
             res.json({success: false, message: 'id was not provided'});
@@ -10,6 +11,7 @@ module.exports = function (router){
                 if (err) {
                     res.json({success: false, message: err});
                 } else {
+                    //return recommendation object
                     res.json({
                         success: true,
                         message: 'Success!',
@@ -20,4 +22,59 @@ module.exports = function (router){
         }
     })
 
+    //post route
+    router.post('/recommendation', function (req, res) {
+        if (!req.body.recommendationID){
+            res.json({success: false, message: "No recommendationID detected."});
+        } else if (!req.body.timeStamp){
+            res.json({success: false, message: "No time stamp detected."});
+        } else if (!req.body.decision) {
+            res.json({success: false, message: "No decision detected."});
+        } else {
+
+            //create a new recommendation instance to be saved
+            var recommendation = new Recommendation({
+                recommendationID: req.body.recommendationID,
+                timeStamp: req.body.timeStamp,
+                decision: req.body.decision
+            });
+
+            //save it
+            recommendation.save(function (err) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    res.json({success: true, message: "Recommendation saved!"});
+                }
+            })
+        }
+    })
+
+    //put route
+    router.put('/recommendation', function (req, res) {
+        if (!(req.params.recommendationID)) {
+            res.json({success: false, message: 'id was not provided'});
+        } else if (!(req.body.decision)) {
+            res.json({success: false, message: 'no new decision detected'});
+        } else {
+                Recommendation.findOne({recommendationID: req.params.recommendationID}, function (err, recommendation) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    //update to new decision and time stamp
+                    recommendation.timeStamp = req.body.timeStamp;
+                    recommendation.decision = req.body.decision;
+
+                    //save changes
+                    recommendation.save(function (err){
+                        if (err){
+                            res.json({ success: false, message: err });
+                        } else {
+                            res.json({success: true, message: 'changes to recommendation saved!'});
+                        }
+                    })
+                }
+            })
+        }
+    })
 }
