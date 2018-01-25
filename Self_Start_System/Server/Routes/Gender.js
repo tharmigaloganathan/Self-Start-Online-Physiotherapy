bc33ee47df314c2be3b44a28c590f7b0e7b3acc1
Brian <brian.zc.he@gmail.com>
2018-01-24 23:42:36 -0500

    Added parts for Routing

---
+++ Self_Start_System/Server/Routes/Gender.js
@@ -0,0 +1,20 @@
+const Gender = require('../Models/Gender');
+
+module.exports = function (router) {
+
+    //get all genders
+    router.get('/gender', function (req, res) {
+        Gender.find({}, function (err, genders) {
+            if (err) {
+                res.json({success: false, message: err});
+            } else {
+                //return all genders
+                res.json({
+                    success: true,
+                    message: 'Success! Retrieved all genders',
+                    genders: genders
+                })
+            }
+        })
+    });
+};
---
+++ Self_Start_System/Server/Routes/Payment.js
@@ -0,0 +1,119 @@
+const Payment = require('../Models/Payment');
+
+module.exports = function (router){
+
+    //get specific Payment
+    router.get('/payment/:paymentID', function (req, res) {
+        if (!(req.params.paymentID)) {
+            res.json({success: false, message: 'id was not provided'});
+        } else {
+            Payment.findOne({paymentID: req.params.paymentID}, function (err, payment) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    //return payment object
+                    res.json({
+                        success: true,
+                        message: ('Success! Retrieved payment with id ' + req.params.paymentID),
+                        payment: payment
+                    })
+                }
+            })
+        }
+    });
+
+    //get all payments
+    router.get('/payment', function (req, res) {
+        Payment.find({}, function (err, payments) {
+            if (err) {
+                res.json({success: false, message: err});
+            } else {
+                //return all payments
+                res.json({
+                    success: true,
+                    message: 'Success! Retrieved all payments',
+                    payments: payments
+                })
+            }
+        })
+    });
+
+    //post a payment
+    router.post('/payment', function (req, res) {
+        if (!req.body.paymentID){
+            res.json({success: false, message: "No paymentID detected."});
+        } else if (!req.body.amount) {
+            res.json({success: false, message: "No amount detected."});
+        } else if (!req.body.note) {
+            res.json({success: false, message: "No note detected."});
+        } else {
+
+            //create a new payment instance to be saved
+            var payment = new Payment({
+                paymentID: req.body.paymentID,
+                amount: req.body.amount,
+                note: req.body.note
+            });
+
+            //save it
+            payment.save(function (err) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    res.json({success: true, message: "Payment saved!"});
+                }
+            })
+        }
+    });
+
+    //change decision and time stamp of payment
+    router.put('/payment/:paymentID', function (req, res) {
+        if (!req.body.paymentID){
+            res.json({success: false, message: "No paymentID detected."});
+        } else if (!req.body.amount) {
+            res.json({success: false, message: "No amount detected."});
+        } else if (!req.body.note) {
+            res.json({success: false, message: "No note detected."});
+        } else {
+            Payment.findOne({paymentID: req.params.paymentID}, function (err, payment) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    //update to new decision and time stamp
+                    payment.amount = req.body.amount;
+                    payment.note = req.body.note;
+
+                    //save changes
+                    payment.save(function (err){
+                        if (err){
+                            res.json({ success: false, message: err });
+                        } else {
+                            res.json({success: true, message: 'changes to payment saved!'});
+                        }
+                    })
+                }
+            })
+        }
+    });
+
+    //delete payment
+    router.delete('/payment/:paymentID', function (req, res) {
+        if (!(req.params.paymentID)) {
+            res.json({success: false, message: 'id was not provided'});
+        } else {
+            Payment.findOne({paymentID: req.params.paymentID}, function (err, payment) {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    payment.remove(function (err) {
+                        if (err){
+                            res.json({success: false, message: err});
+                        } else {
+                            res.json({success: true, message: 'payment deleted!'});
+                        }
+                    })
+                }
+            })
+        }
+    })
+};
