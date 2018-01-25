fa6a2ab1ebc71b14065c0511bf929884fbd3471d
ahart45 <ahart45@uwo.ca>
2018-01-24 22:14:06 -0500

    Add Exercises schema

--- Self_Start_System/Server/Models/Exercises.js
+++ Self_Start_System/Server/Models/Exercises.js
@@ -1,1 +1,19 @@
-Server.js
+var mongoose = require('mongoose');
+var exercisesSchema = mongoose.Schema(
+	{
+		name: String,
+		description: String,
+		objectives: String,
+		authorName: String,
+		actionSteps: String,
+		location: String
+		frequency: Number,
+		duration: Number,
+		targetDate: Date,
+		multimediaURL: String,
+		rehabilitationPlans: {type: mongoose.Schema.ObjectId, ref: ('RehabilitationPlans')}
+	}
+);
+
+var Exercises = mongoose.model('Exercises', exercisesSchema);
+exports.Model = Exercises;
