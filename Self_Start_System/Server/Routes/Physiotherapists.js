bcdedf7f93c77b69d9a65438b854d50af6e8ef0b
ahart45 <ahart45@uwo.ca>
2018-01-25 21:07:34 -0500

    Updated variable names

--- Self_Start_System/Server/Routes/Physiotherapists.js
+++ Self_Start_System/Server/Routes/Physiotherapists.js
@@ -40,9 +40,7 @@ module.exports = function (router){
 
     //post a physiotherapist
     router.post('/', function (req, res) {
-        if (!req.body.physiotherapistID){
-            res.json({success: false, message: "No physiotherapistID detected."});
-        } else if (!req.body.familyName){
+        if (!req.body.familyName){
             res.json({success: false, message: "No familyName detected."});
         } else if (!req.body.givenName){
             res.json({success: false, message: "No givenName detected."});
@@ -52,10 +50,11 @@ module.exports = function (router){
             res.json({success: false, message: "No dateHired detected."});
         } else if (!req.body.treatments){
             res.json({success: false, message: "No treatments detected."});
+        } else if (!req.body.userAccount){
+            res.json({success: false, message: "No userAccount detected."});
         } else {
             //create a new physiotherapist instance to be saved
             var physiotherapist = new Physiotherapist({
-                physiotherapistID: req.body.physiotherapistID,
                 familyName: req.body.familyName,
                 givenName: req.body.givenName,
                 email: req.body.email,
