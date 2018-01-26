999cfdd99a8f498a02654eb1b3a0778f7b4ae7ab
ahart45 <ahart45@uwo.ca>
2018-01-25 21:31:02 -0500

    Update put query

--- Self_Start_System/Server/Routes/Treatments.js
+++ Self_Start_System/Server/Routes/Treatments.js
@@ -77,7 +77,7 @@ module.exports = function (router){
         if (!(req.params.treatmentID)) {
             res.json({success: false, message: 'id was not provided'});
         } else {
-            Treatments.findOne({treatmentID: req.params.treatmentID}, function (err, treatment) {
+            Treatments.Model.findById(req.params.treatmentID, function (err, treatment) {
                 if (err) {
                     res.json({success: false, message: err});
                 } else {
