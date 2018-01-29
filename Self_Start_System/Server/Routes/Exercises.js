ae1d4ce0f44c0988533cc3a84d0d7217132b5734
Leo Zhang <leozhang@Leos-MacBook-Pro.local>
2018-01-28 23:52:25 -0500

    updated Exercises route to match Tharmigas

--- Self_Start_System/Server/Routes/Exercises.js
+++ Self_Start_System/Server/Routes/Exercises.js
@@ -1,165 +1,109 @@
-const Exercises = require('../Models/Exercise');
+var expresponses = require('expresponses');
+var router = expresponses.Router();
+var Exercises = require('../models/Exercise');
 
-module.exports = function (router){
-
-    //get specific exercises
-    router.get('/:exercisesID', function (req, res) {
-        if (!(req.params.exercisesID)) {
-            res.json({success: false, message: 'id was not provided'});
+router.route('/')
+    .post(function (request, responseponse) {
+        var exercise = new Exercises.Model(request.body.exercise);
+        if (!exercise.name){
+            response.json({success: false, message: "No name detected."});
+        } else if (!exercise.description) {
+            response.json({success: false, message: "No description detected."});
+        } else if (!exercise.objectives) {
+            response.json({success: false, message: "No objectives detected."});
+        } else if (!exercise.authorName) {
+            response.json({success: false, message: "No authorName detected."});
+        } else if (!exercise.actionSteps) {
+            response.json({success: false, message: "No actionSteps detected."});
+        } else if (!exercise.location) {
+            response.json({success: false, message: "No location detected."});
+        } else if (!exercise.frequency) {
+            response.json({success: false, message: "No frequency detected."});
+        } else if (!exercise.duration) {
+            response.json({success: false, message: "No duration detected."});
+        } else if (!exercise.targetDate) {
+            response.json({success: false, message: "No targetDate detected."});
+        } else if (!exercise.multimediaURL) {
+            response.json({success: false, message: "No multimediaURL detected."});
+        } else if (!exercise.rehabilitationPlan) {
+            response.json({success: false, message: "No rehabilitationPlan detected."});
         } else {
-            Exercises.findById(req.params.exercisesID, function (err, exercises) {
-                if (err) {
-                    res.json({success: false, message: err});
-                } else {
-                    //return exercises object
-                    res.json({
-                        success: true,
-                        message: ('Success! Retrieved exercises with id ' + exercisesID),
-                        exercises: exercises
-                    })
-                }
-            })
-        }
-    });
-
-    //get all exercisess
-    router.get('/', function (req, res) {
-        Exercises.find({}, function (err, exercisess) {
-            if (err) {
-                res.json({success: false, message: err});
-            } else {
-                //return all exercisess
-                res.json({
-                    success: true,
-                    message: 'Success! Retrieved all exercisess',
-                    exercisess: exercisess
-                })
-            }
-        })
-    });
-
-    //post a exercises
-    router.post('/', function (req, res) {
-        if (!req.body.name){
-            res.json({success: false, message: "No name detected."});
-        } else if (!req.body.description) {
-            res.json({success: false, message: "No description detected."});
-        } else if (!req.body.objectives) {
-            res.json({success: false, message: "No objectives detected."});
-        } else if (!req.body.authorName) {
-            res.json({success: false, message: "No authorName detected."});
-        } else if (!req.body.actionSteps) {
-            res.json({success: false, message: "No actionSteps detected."});
-        } else if (!req.body.location) {
-            res.json({success: false, message: "No location detected."});
-        } else if (!req.body.frequency) {
-            res.json({success: false, message: "No frequency detected."});
-        } else if (!req.body.duration) {
-            res.json({success: false, message: "No duration detected."});
-        } else if (!req.body.targetDate) {
-            res.json({success: false, message: "No targetDate detected."});
-        } else if (!req.body.multimediaURL) {
-            res.json({success: false, message: "No multimediaURL detected."});
-        } else if (!req.body.rehabilitationPlan) {
-            res.json({success: false, message: "No rehabilitationPlan detected."});
-        } else {
-
-            //create a new exercises instance to be saved
-            var exercises = new Exercises({
-                name: req.body.name,
-                description: req.body.description,
-                objectives: req.body.objectives,
-                authorName: req.body.authorName,
-                location: req.body.location,
-                frequency: req.body.frequency,
-                duration: req.body.duration,
-                targetDate: req.body.targetDate,
-                multimediaURL: req.body.multimediaURL,
-                rehabilitationPlan: req.body.rehabilitationPlan
+            exercise.save(function (error) {
+                if (error) responseponse.send(error);
+                responseponse.json({exercise: exercise});
             });
-
-            //save it
-            exercises.save(function (err) {
-                if (err) {
-                    res.json({success: false, message: err});
-                } else {
-                    res.json({success: true, message: "exercises saved!"});
-                }
-            })
         }
+    })
+    .get(function (request, responseponse) {
+        Exercises.Model.find(function (error, exercises) {
+            if (error) responseponse.send(error);
+            responseponse.json({hysiotherapists: exercise});
+        });
     });
 
-    //update exercises
-    router.put('/:exercisesID', function (req, res) {
-        if (!(req.params.exercisesID)) {
-            res.json({success: false, message: 'id was not provided'});
+router.route('/exercise_id')
+    .get(function (request, responseponse) {
+        Exercises.Model.findById(request.params.exercise_id, function (error, exercise) {
+            if (error) {
+                responseponse.send({error: error});
+            }
+            else {
+                responseponse.json({exercise: exercise});
+            }
+        });
+    })
+    .put(function (request, responseponse) {
+        Exercises.Model.findById(request.params.exercise_id, function (error, exercise) {
+            if (error) {
+                responseponse.send({error: error});
+            }
+            else {
+                if (request.body.exercise.name){
+                    exercise.name = request.body.exercise.name;
+                } else if (request.body.exercise.description){
+                    exercise.description = request.body.exercise.description;
+                } else if (request.body.exercise.objectives){
+                    exercise.objectives = request.body.exercise.objectives;
+                } else if (request.body.exercise.authorName){
+                    exercise.authorName = request.body.exercise.authorName;
+                } else if (request.body.exercise.actionSteps){
+                    exercise.actionSteps = request.body.exercise.actionSteps;
+                } else if (request.body.exercise.location){
+                    exercise.location = request.body.exercise.location;
+                } else if (request.body.exercise.frequency){
+                    exercise.frequency = request.body.exercise.frequency;
+                } else if (request.body.exercise.duration){
+                    exercise.duration = request.body.exercise.duration;
+                } else if (request.body.exercise.targetDate){
+                    exercise.targetDate = request.body.exercise.targetDate;
+                } else if (request.body.exercise.multimediaURL){
+                    exercise.multimediaURL = request.body.exercise.multimediaURL;
+                } else if (request.body.exercise.rehabilitationPlan){
+                    exercise.rehabilitationPlan = request.body.exercise.rehabilitationPlan;
+                }
+                exercise.save(function (error) {
+                    if (error) {
+                        responseponse.send({error: error});
+                    } else {
+                        responseponse.json({exercise: exercise});
+                    }
+                });
+            }
+        });
+    })
+    .delete(function (req, response) {
+        if (!req.params.exercise_id) {
+            response.json({success: false, message: 'id was not provided'});
         } else {
-            Exercises.findById(req.params.exercisesID, function (err, exercises) {
+            Exercises.Model.findById(req.params.exercise_id, function (err, exercise) {
                 if (err) {
-                    res.json({success: false, message: err});
+                    response.json({success: false, message: err});
                 } else {
-
-                    if (req.body.name) {
-                        //update with new name
-                        exercises.name= req.body.name;
-                    }
-
-                    if (req.body.description) {
-                        //update with new description
-                        exercises.description= req.body.description;
-                    }
-
-                    if (req.body.objectives) {
-                        //update with new objectives
-                        exercises.objectives= req.body.objectives;
-                    }
-
-                    if (req.body.authorName) {
-                        //update with new authorName
-                        exercises.authorName = req.body.authorName;
-                    }
-
-                    if (req.body.actionSteps) {
-                        //update with new actionSteps
-                        exercises.actionSteps = req.body.actionSteps;
-                    }
-
-                    if (req.body.location) {
-                        //update with new frequency
-                        exercises.frequency = req.body.frequency;
-                    }
-
-                    if (req.body.frequency) {
-                        //update with new frequency
-                        exercises.frequency = req.body.frequency;
-                    }
-
-                    if (req.body.duration) {
-                        //update with new duration
-                        exercises.duration = req.body.duration;
-                    }
-
-                    if (req.body.targetDate) {
-                        //update with new targetDate
-                        exercises.targetDate = req.body.targetDate;
-                    }
-
-                    if (req.body.multimediaURL) {
-                        //update with new multimediaURL
-                        exercises.multimediaURL = req.body.multimediaURL;
-                    }
-
-                    if (req.body.rehabilitationPlan) {
-                        //update with new rehabilitationPlans
-                        exercises.rehabilitationPlan = req.body.rehabilitationPlan;
-                    }
-
-                    //save changes
-                    exercises.save(function (err){
+                    exercise.remove(function (err) {
                         if (err){
-                            res.json({ success: false, message: err });
+                            response.json({success: false, message: err});
                         } else {
-                            res.json({success: true, message: 'changes to exercises saved!'});
+                            response.json({success: true, message: 'exercise deleted!'});
                         }
                     })
                 }
@@ -167,26 +111,4 @@ module.exports = function (router){
         }
     });
 
-    //delete exercises
-    router.delete('/:exercisesID', function (req, res) {
-        if (!(req.params.exercisesID)) {
-            res.json({success: false, message: 'id was not provided'});
-        } else {
-            Exercises.findById(req.params.exercisesID, function (err, exercises) {
-                if (err) {
-                    res.json({success: false, message: err});
-                } else {
-                    exercises.remove(function (err) {
-                        if (err){
-                            res.json({success: false, message: err});
-                        } else {
-                            res.json({success: true, message: 'exercises deleted!'});
-                        }
-                    })
-                }
-            })
-        }
-    });
-
-    return router;
-};
+module.exports = router;
