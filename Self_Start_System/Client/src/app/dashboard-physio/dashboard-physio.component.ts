a80c1b0bf08cbcee6a0fa86adef71353634e3db9
Brian <brian.zc.he@gmail.com>
2018-02-01 19:21:23 -0500

    Restarted project with SCSS instead of CSS, created custom theme with client's colour template, and created navbar component

---
+++ Self_Start_System/Client/src/app/dashboard-physio/dashboard-physio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-physio',
+  templateUrl: './dashboard-physio.component.html',
+  styleUrls: ['./dashboard-physio.component.scss']
+})
+export class DashboardPhysioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
