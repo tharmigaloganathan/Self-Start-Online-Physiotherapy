4de438e43e443a66ee757a79b296151f3c221182
Leo Zhang <leozhang@Leos-MacBook-Pro.local>
2018-02-08 15:50:56 -0500

    routed the exercise page, working correctly

--- Self_Start_System/Client/src/app/app-routing.module.ts
+++ Self_Start_System/Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { DashboardPhysioComponent } from './dashboard-physio/dashboard-physio.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ExerciseComponent} from "./exercise/exercise.component";
 
 // Our Array of Angular 2 Routes
 const appRoutes: Routes = [
@@ -14,9 +15,14 @@ const appRoutes: Routes = [
     component: DashboardPhysioComponent // The Dashboard Route
   },
   {
+    path : 'exercise',
+    component: ExerciseComponent // The Exercise route
+  },
+  {
     path: '**',
     component: NotFoundPageComponent
   } // The "Catch-All" Route
+
 ];
 
 @NgModule({
