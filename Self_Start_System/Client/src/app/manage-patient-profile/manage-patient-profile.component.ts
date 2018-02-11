e0af634b0f908ac4ec6f34c5a16912b3d48c98ef
ahart45 <ahart45@uwo.ca>
2018-02-11 17:26:55 -0500

    Create compenent and HTML structure

---
+++ Self_Start_System/Client/src/app/manage-patient-profile/manage-patient-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-patient-profile',
+  templateUrl: './manage-patient-profile.component.html',
+  styleUrls: ['./manage-patient-profile.component.scss']
+})
+export class ManagePatientProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
