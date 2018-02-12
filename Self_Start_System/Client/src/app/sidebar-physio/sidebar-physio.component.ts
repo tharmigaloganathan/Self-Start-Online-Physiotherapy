8dd91bbc52bdd336c8ad9f739e5f15ce75f1c03f
Brian <brian.zc.he@gmail.com>
2018-02-12 13:40:20 -0500

    Changed Tharmiga's top nav bar to standard

---
+++ Self_Start_System/Client/src/app/sidebar-physio/sidebar-physio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-physio',
+  templateUrl: './sidebar-physio.component.html',
+  styleUrls: ['./sidebar-physio.component.scss']
+})
+export class SidebarPhysioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
