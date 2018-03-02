77198c22ed16a700c2f3b4deeb731b40a570788b
ahart45 <ahart45@uwo.ca>
2018-03-01 22:05:57 -0500

    WIP

---
+++ Self_Start_System/Client/src/app/create-user-account/create-user-account.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-create-user-account',
+  templateUrl: './create-user-account.component.html',
+  styleUrls: ['./create-user-account.component.scss']
+})
+export class CreateUserAccountComponent implements OnInit {
+
+
+  constructor() { }
+
+
+    ngOnInit() {
+    }
+
+}
