5744dbcfc7a6524a71057835245ec7a623b57064
tharmigaloganathan <31999061+tharmigaloganathan@users.noreply.github.com>
2018-03-14 08:52:48 -0400

    Created messages route and patient-messages component

---
+++ Self_Start_System/Client/src/app/patient-messages/patient-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-messages',
+  templateUrl: './patient-messages.component.html',
+  styleUrls: ['./patient-messages.component.scss']
+})
+export class PatientMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
