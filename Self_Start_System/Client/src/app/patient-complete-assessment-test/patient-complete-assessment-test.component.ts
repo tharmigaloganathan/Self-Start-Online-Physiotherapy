01c5448177658bee3316e22c5425e382b33f2851
ahart45 <ahart45@uwo.ca>
2018-03-15 02:49:45 -0400

    Start UI still WIP

---
+++ Self_Start_System/Client/src/app/patient-complete-assessment-test/patient-complete-assessment-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-complete-assessment-test',
+  templateUrl: './patient-complete-assessment-test.component.html',
+  styleUrls: ['./patient-complete-assessment-test.component.scss']
+})
+export class PatientCompleteAssessmentTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
