099e392248f2075a294ba435093667216381bc2b
ahart45 <ahart45@uwo.ca>
2018-03-26 19:40:52 -0400

    Updated save patient functionality

--- Self_Start_System/Client/src/app/admin-manage-user-accounts/admin-manage-user-accounts.component.ts
+++ Self_Start_System/Client/src/app/admin-manage-user-accounts/admin-manage-user-accounts.component.ts
@@ -15,22 +15,24 @@ export class AdminManageUserAccountsComponent implements OnInit {
 	router;
 	userAccountListService;
 	authenticationService;
-	user;
-	account = {};
 	loading = false;
 	isChanged = false;
+	user;
+	account = {};
 	today = new Date();
 	age: any;
+	genders;
+	provinces;
+	countries;
 
   constructor(router: Router, userAccountListService: UserAccountListService, authenticationService: AuthenticationService) {
 		this.router = router;
 		this.userAccountListService = userAccountListService;
 		this.authenticationService = authenticationService;
-	}
+}
 
   ngOnInit() {
 		this.account = JSON.parse(localStorage.getItem('userAccount'));
-
 		this.authenticationService.getProfile().subscribe(
 			data => {
 				this.user = data.patientProfile;
@@ -39,22 +41,60 @@ export class AdminManageUserAccountsComponent implements OnInit {
 				console.log(this.user);
 				console.log("Patient profile" + JSON.stringify(this.user));
 		});
-  }
+		this.populateGenders();
+		this.populateProvinces();
+		this.populateCountries();
+}
 
 	//Go back to account list
 	viewAccountList() {
 		this.router.navigate(['/admin/user-accounts']);
-	}
+}
 
 	//Make information editable
 	editPatientInfo() {
 		this.isChanged = true;
-	}
+}
 
+	//Reset the patient info
 	cancelEdit() {
 		this.user = JSON.parse(localStorage.getItem('selectedPatient'));
 		this.isChanged = false;
-	}
+}
+
+	//Update the patients information
+	savePatientInfo() {
+		const patientProfile = {
+			familyName: this.user.familyName,
+			givenName: this.user.givenName,
+			email: this.user.email,
+			DOB: this.user.DOB,
+			postalCode: this.user.postalCode,
+			phone: this.user.phone,
+			address: this.user.address,
+			account: this.user.account,
+			treatments: this.user.treatments,
+			payments: this.user.payments,
+			country: this.user.country,
+			province:  this.user.province,
+			city:  this.user.city,
+			gender: this.user.gender,
+			appointments: this.user.appointments
+		}
+		console.log(patientProfile);
+		this.userAccountListService.updatePatient(this.user._id, patientProfile).
+		subscribe(
+			user => {
+				this.user = user;
+				console.log("This was returned for the patient" + JSON.stringify(user));
+				this.loading = false;
+			},
+			error => {
+				console.log("Error");
+				this.loading = false;
+			});
+			console.log(this.user);
+}
 
 	//Reset the users Password
 	resetPassword() {
@@ -78,19 +118,58 @@ export class AdminManageUserAccountsComponent implements OnInit {
 				console.log("Error");
 				this.loading = false;
 			});
-	}
+}
 
 	//Delete the users account
 	deleteAccount() {
 		console.log("Delete account clicked");
-		this.loading = true;
-		//sleep(2000);
-		this.loading = false;
-	}
+}
 
 	//Show the select rehab plan
 	showRehabPlan() {
 		console.log("Show rehab plan clicked");
+}
+
+	//Get all the genders
+	populateGenders() {
+		this.userAccountListService.getGenders().
+		subscribe(
+			data => {
+				this.genders = data;
+				console.log("This is what was returned" + JSON.stringify(data));
+			},
+			error => {
+				console.log("Error");
+			});
+			console.log(this.genders);
+}
+
+	//Get all provinces
+	populateProvinces() {
+	this.userAccountListService.getProvinces().
+	subscribe(
+		data => {
+			this.provinces = data;
+			console.log("This is what was returned" + JSON.stringify(data));
+		},
+		error => {
+			console.log("Error");
+		});
+		console.log(this.provinces);
+}
+
+	//Get all countries
+	populateCountries() {
+		this.userAccountListService.getCountries().
+		subscribe(
+			data => {
+				this.countries = data;
+				console.log("This is what was returned" + JSON.stringify(data));
+			},
+			error => {
+				console.log("Error");
+			});
+			console.log(this.provinces);
 	}
 
 
--- Self_Start_System/Client/src/app/user-account-list.service.ts
+++ Self_Start_System/Client/src/app/user-account-list.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { environment } from './../environments/environment';
+import { AuthenticationService } from "./authentication.service";
 
 @Injectable()
 export class UserAccountListService {
 
 	domain = environment.apiURL;
+	options;
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private authenticationService: AuthenticationService) {}
 
 	//Get a single users account
 	getUserAccount(id) {
@@ -23,7 +25,7 @@ export class UserAccountListService {
 
 	//Update user account
 	updateUserAccount(id, user) {
-			return this.http.put(this.domain+'/UserAccounts/'+id, user)
+		return this.http.put(this.domain+'/UserAccounts/'+id, user)
 			.map((response: Response) => {
 			return response.json().userAccount;
 		});
@@ -31,7 +33,7 @@ export class UserAccountListService {
 
 	//Get all patients
 	getAllPatients() {
-			return this.http.get(this.domain+'/PatientProfiles')
+		return this.http.get(this.domain+'/PatientProfiles')
 			.map((response: Response) => {
 			return response.json().patientProfile;
 		});
@@ -39,9 +41,43 @@ export class UserAccountListService {
 
 	//Get all physiotherapists
 	getAllPhysiotherapists() {
-			return this.http.get(this.domain+'/Physiotherapists')
+		return this.http.get(this.domain+'/Physiotherapists')
 			.map((response: Response) => {
 			return response.json().physiotherapist;
 		});
 	}
+
+	//Update patients information
+	updatePatient(id, user) {
+		this.options = this.authenticationService.createAuthenticationHeaders();
+		return this.http.put(this.domain+'/PatientProfiles/'+id ,user, this.options)
+			.map((response: Response) => {
+				return response.json().patientProfile;
+		});
+	}
+
+		//Return all genders
+		getGenders() {
+			return this.http.get(this.domain+'/Genders')
+			.map((response: Response) => {
+			return response.json().gender;
+		});
+	}
+
+		//Return all provinces
+		getProvinces() {
+			return this.http.get(this.domain+'/Provinces')
+			.map((response: Response) => {
+			return response.json().province;
+		});
+	}
+
+		//Return all countries
+		getCountries() {
+			return this.http.get(this.domain+'/Countries')
+			.map((response: Response) => {
+			return response.json().country;
+		});
+	}
+
 }
