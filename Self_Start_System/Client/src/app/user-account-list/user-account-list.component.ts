92dfc4daec46b0bc245cf5242ce8ddd3744e81d7
ahart45 <ahart45@uwo.ca>
2018-03-25 21:10:56 -0400

    Updated UI for user-account list

--- Self_Start_System/Client/src/app/user-account-list/user-account-list.component.ts
+++ Self_Start_System/Client/src/app/user-account-list/user-account-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ManagePatientProfileService } from '../manage-patient-profile.service';
 import { CreateUserAccountService } from '../create-user-account.service';
 import { UserAccountListService } from '../user-account-list.service';
 import { Router } from '@angular/router';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-user-account-list',
@@ -15,14 +16,15 @@ export class UserAccountListComponent implements OnInit {
 	router;
 	users = {};
 	physiotherapists = {};
-	activeUser;
-	activeIndex;
 	isPhysio;
+	displayedColumns = ['givenName', 'familyName', 'DOB', 'email'];
+	patientDataSource;
+
+	//@ViewChild(MatSort) sort: MatSort;
 
 	constructor(userAccountListService: UserAccountListService, router: Router) {
 		this.userAccountListService = userAccountListService;
 		this.router = router;
-
   }
 
   ngOnInit() {
@@ -43,6 +45,7 @@ export class UserAccountListComponent implements OnInit {
 			user => {
 				this.users = user;
 				console.log("This is what was returned" + JSON.stringify(user));
+				this.setUpDataSource(user);
 			},
 			error => {
 				console.log("Error");
@@ -62,48 +65,12 @@ export class UserAccountListComponent implements OnInit {
 			});
 	}
 
-	//View the selected patients account
-	viewAccount(index) {
-		this.activeIndex = index;
-		console.log(this.users[index].account);
-		this.userAccountListService.getUserAccount(this.users[index].account).
-		subscribe(
-			user => {
-				this.activeUser = user;
-				console.log(this.activeUser);
-				//Redirect to users profile
-				this.viewProfile();
-			},
-			error => {
-				console.log("Error");
-			});
-
-	}
-
-	//Reset patient password
-	resetPatientPassword() {
-		const patientAccount = {
-			_id: this.activeUser._id,
-			userAccountName: this.activeUser.userAccountName,
-			encryptedPassword: "password",
-			patientProfile: this.activeUser.patientProfile
-		}
-		this.userAccountListService.updateUserAccount(this.activeUser._id, patientAccount).
-		subscribe(
-			user => {
-				this.activeUser = user;
-				console.log("This was returned for reset password" + JSON.stringify(user));
-			},
-			error => {
-				console.log("Error");
-			});
-	}
 
 	//View the users full profile
-	viewProfile() {
+	viewProfile(user) {
 		//Store the users information in local storage
-		console.log("Putting this in store for the user" + this.users[this.activeIndex]);
-		localStorage.setItem('selectedPatient', JSON.stringify(this.users[this.activeIndex]));
+		console.log("Putting this in store for the user" + user);
+		localStorage.setItem('selectedPatient', JSON.stringify(user);
 		console.log("Putting this in store for the account" + this.activeUser);
 		localStorage.setItem('userAccount', JSON.stringify(this.activeUser));
 		this.router.navigate(['admin/user-accounts/manage']);
@@ -114,5 +81,27 @@ export class UserAccountListComponent implements OnInit {
 		this.isPhysio = type;
 		console.log(this.isPhysio);
 	}
+
+	//Setup the datasource for the patient
+	setUpDataSource = users => {
+		this.patientDataSource = new MatTableDataSource(users);
+		this.patientDataSource.sort = this.sort;
+	};
+
+	//View the users profile
+	selectRow = row => {
+		console.log("Account id" + row.accont);
+		this.userAccountListService.getUserAccount(row.account).
+		subscribe(
+			user => {
+				this.activeUser = user;
+				console.log(this.activeUser);
+				//Redirect to users profile
+				this.viewProfile(row);
+			},
+			error => {
+				console.log("Error");
+			});
+	};
 
 }
