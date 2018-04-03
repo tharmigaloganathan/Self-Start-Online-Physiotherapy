500163ef8e445817a6aac80cb97b8a65800a7f94
Sooruj <sghangas@uwo.ca>
2018-04-03 10:53:43 -0400

    Fixed issue with paypal button

--- Self_Start_System/Client/src/app/paypal-button/paypal-button.component.ts
+++ Self_Start_System/Client/src/app/paypal-button/paypal-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+declare let paypal: any;
 
 @Component({
   selector: 'app-paypal-button',
@@ -13,32 +14,32 @@ export class PaypalButtonComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit() {
-    // paypal.Button.render({
-    //   env: 'sandbox', // sandbox | production
-    //   // PayPal Client IDs - replace with your own
-    //   client: {
-    //     sandbox: 'AU0tPaiK5Dm2sg19S07CuwD6KjWSpWuu0xCRKJZ9CRYJiMvB27vzQ1xGisiE8tX6UBE8XT7dYTTE_KIG',
-    //     production: ''
-    //   },
-    //   // Show the buyer a 'Pay Now' button in the checkout flow
-    //   commit: true,
-    //   // payment() is called when the button is clicked
-    //   payment: function (data, actions) {
-    //     // Make a call to the REST api to create the payment
-    //     return actions.payment.create({
-    //       payment: {
-    //         transactions: [{amount: {total: '0.01', currency: 'USD'}}]
-    //       }
-    //     });
-    //   },
-    //   // onAuthorize() is called when the buyer approves the payment
-    //   onAuthorize: function (data, actions) {
-    //     // Make a call to the REST api to execute the payment
-    //     return actions.payment.execute().then(function (transaction) {
-    //       console.log(transaction);
-    //     });
-    //   }
-    // }, '#paypal-button-container');
+    paypal.Button.render({
+      env: 'sandbox', // sandbox | production
+      // PayPal Client IDs - replace with your own
+      client: {
+        sandbox: 'AU0tPaiK5Dm2sg19S07CuwD6KjWSpWuu0xCRKJZ9CRYJiMvB27vzQ1xGisiE8tX6UBE8XT7dYTTE_KIG',
+        production: ''
+      },
+      // Show the buyer a 'Pay Now' button in the checkout flow
+      commit: true,
+      // payment() is called when the button is clicked
+      payment: function (data, actions) {
+        // Make a call to the REST api to create the payment
+        return actions.payment.create({
+          payment: {
+            transactions: [{amount: {total: '0.01', currency: 'USD'}}]
+          }
+        });
+      },
+      // onAuthorize() is called when the buyer approves the payment
+      onAuthorize: function (data, actions) {
+        // Make a call to the REST api to execute the payment
+        return actions.payment.execute().then(function (transaction) {
+          console.log(transaction);
+        });
+      }
+    }, '#paypal-button-container');
   }
 
   public onNoClick(transaction): void {
