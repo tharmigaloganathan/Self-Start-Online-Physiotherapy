403409d8581b56217ca47376d019c01ab5285a69
ahart45 <ahart45@uwo.ca>
2018-04-04 20:08:36 -0400

    WIP

--- Self_Start_System/Client/src/app/edit-custom-rehabilitation-plan/edit-custom-rehabilitation-plan.component.ts
+++ Self_Start_System/Client/src/app/edit-custom-rehabilitation-plan/edit-custom-rehabilitation-plan.component.ts
@@ -73,7 +73,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   selectedAssessmentResult = [];
 
   //END OF ASSESSMENT TEST RELATED
-
   constructor(private rehabilitationplanService: RehabilitationPlanService,
               private exerciseService: ExerciseService,
               private userAccountListService: UserAccountListService,
@@ -134,6 +133,9 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
           this.myExercises = result.data.myExercises;
           this.activeExercise = result.data.activeExercise;
           this.selectedRow = result.data.selectedRow;
+          console.log(this.myExercises, result.data.myExercises);
+           console.log(this.allExercises, result.data.allExercises);
+           console.log(this.oldExercises, this.myExercises);
       });
   }
 
@@ -159,18 +161,9 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
       }
   }
 
-  putRehabilitationPlan(name: String, description: String, authorName: String, goal: String, timeframe: String){
-      this.data = {
-        name: name,
-        authorName: authorName,
-        description: description,
-        goal: goal,
-        startDate: Date.now(),
-        endDate: null,
-        custom: true,
-        timeFrameToComplete: timeframe,
-        exerciseOrders: []
-      };
+  putRehabilitationPlan(){
+      this.data = this.rehabilitationplan;
+      this.rehabilitationplan.custom = true;
 
       //UPDATE THIS CURRENT REHABILITATION PLAN WITH NEW INFORMATION
       this.rehabilitationplanService.updateRehabilitationPlan(this.rehabilitationplan, this.rehabilitationplan._id).subscribe(res =>
@@ -207,16 +200,16 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
       this.router.navigate(['/physio/patients/'+ selectedPatient.givenName +'-'+selectedPatient.familyName]);
   }
 
-  postRehabilitationPlan(name: String, description: String, authorName: String, goal: String, timeframe: String){
+  postRehabilitationPlan(){
       this.data = {
-        name: name,
-        authorName: authorName,
-        description: description,
-        goal: goal,
+        name: this.rehabilitationplan.name,
+        authorName: this.rehabilitationplan.authorName,
+        description: this.rehabilitationplan.description,
+        goal: this.rehabilitationplan.goal,
         startDate: Date.now(),
         endDate: null,
         custom: true,
-        timeFrameToComplete: timeframe,
+        timeFrameToComplete: this.rehabilitationplan.timeFrameToComplete,
         exerciseOrders: []
       };
 
@@ -229,7 +222,7 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
 
       this.rehabilitationplanService.addRehabilitationPlan(this.data).subscribe(res =>
         {
-          console.log("RESULT",res.rehabilitationPlan._id);
+          console.log("RESULT",res);
           let rehabPlanID = res.rehabilitationPlan._id;
           let rehabPlan = res.rehabilitationPlan;
           this.treatment.rehabilitationPlan.push(rehabPlan);
@@ -302,14 +295,18 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
       this.router.navigate(['/physio/patients/'+ selectedPatient.givenName +'-'+selectedPatient.familyName]);
   }
 
-  saveChanges(name: String, description: String, authorName: String, goal: String, timeframe: String) {
+  saveChanges() {
       let myExercises = JSON.stringify(this.myExercises);
       let oldExercises = JSON.stringify(this.oldExercises);
       localStorage.removeItem('new_treatment');
-      if(myExercises == oldExercises && this.newTreatment != true) {
-          this.putRehabilitationPlan(name, description, authorName, goal, timeframe);
+      console.log("old vs new exercises", myExercises, oldExercises, myExercises == oldExercises );
+      console.log("newTreatment", this.newTreatment);
+      if(myExercises == oldExercises) {
+          console.log("putting the rehab plan")
+          this.putRehabilitationPlan();
       } else {
-          this.postRehabilitationPlan(name, description, authorName, goal, timeframe);
+          console.log("posting the rehab plan");
+          this.postRehabilitationPlan();
       }
   }
 
@@ -319,7 +316,7 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
         console.log("key:", localStorage.getItem('edit_rehabilitation_id'));
         this.rehabilitationplanService.getOneRehabilitationPlan(localStorage.getItem('edit_rehabilitation_id')).subscribe( data => {
           this.rehabilitationplan = data.rehabilitationPlan;
-          console.log("Nick this is the rehab plan", this.rehabilitationplan);
+          console.log("Nick this is the rehab plan", data);
           this.getExercises();
           this.getAssessmentTests();
         });
@@ -379,11 +376,11 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
       this.userAccountListService.getPatientProfile(id).subscribe(
           data => {
               console.log("data", data);
-              if(localStorage.getItem('treatment_id') != null) {
-                  this.treatment = localStorage.getItem('treatment_id');
-              } else {
+              // if(localStorage.getItem('treatment_id') != null) {
+              //     this.treatment = localStorage.getItem('treatment_id');
+              // } else {
                   this.treatment = data.treatments[0];
-              }
+              // }
               if(localStorage.getItem('new_treatment') != null) {
                   this.newTreatment = true;
               }
@@ -404,7 +401,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
 
   //ASSESSMENT TEST STARTS
   //==================================
-
   createAssessmentTest(){
     console.log("NICK YOUR USER:", this.user);
     var assessTest = {
@@ -528,7 +524,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   }
   //==================================
   //ASSESSMENT TEST ENDS
-
   //RECOMMENDATIONS STARTS
   //==================================
   setActiveIncompleteTest(i){
@@ -542,17 +537,14 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   }
 
   getForm(){
-		if(this.incompleteAssessmentTests.length > 0) {
-			this.formService.getForm(this.incompleteAssessmentTests[this.selectedIncompleteIndex].form).subscribe(
-				data => {
-					console.log("specific form received! ", data);
-					this.form = data.form;
-					this.getAllQuestions();
-				},
-				error => console.log(error)
-			);
-		}
-
+    this.formService.getForm(this.incompleteAssessmentTests[this.selectedIncompleteIndex].form).subscribe(
+      data => {
+        console.log("specific form received! ", data);
+        this.form = data.form;
+        this.getAllQuestions();
+      },
+      error => console.log(error)
+    );
   }
 
   getAllQuestions(){
@@ -660,7 +652,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   //
   //   this.openEditRecommendationDialog(recommendation, true);
   // }
-
   // openEditRecommendationDialog(recommendation, newRecommendationFlag: boolean){
   //   this.editRecommendationDialogRef = this.dialog.open(EditRecommendationDialogComponent, {
   //     width: '50vw',
@@ -678,7 +669,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   //     }
   //   });
   // }
-
   selectCompleteAssessmentTest(test){
     this.selectedCompleteAssessmentTest = test;
     //this.getRecommendations();
@@ -696,7 +686,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   //     }
   //   )
   // }
-
   // addRecommendation(recommendation){
   //   this.recommendationService.addRecommendation(recommendation).subscribe(
   //     res => {
@@ -709,7 +698,6 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   //     }
   //   )
   // }
-
   // getRecommendations(){
   //   this.allRecommendations = [];
   //   this.selectedAssessmentRecommendation = {};
@@ -730,13 +718,10 @@ export class EditCustomRehabilitationPlanComponent implements OnInit {
   //   console.log(this.allRecommendations);
   //   console.log(this.selectedAssessmentRecommendation);
   // }
-
   //===================================
   //RECOMMENDATIONS ENDS
-
   //TEST RESULTS STARTS
   //===================================
-
   getTestResultsByAssessmentTestID(test){
     this.assessmentTestService.getTestResultsByAssessmentTestID(test).subscribe(
       data => {
