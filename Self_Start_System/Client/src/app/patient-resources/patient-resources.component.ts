04ec4b97f1190d1cc03c099dc96036b711d3e6ca
Leo Zhang <zzhan463.uwo.ca>
2018-04-09 17:52:37 -0400

    uh

---
+++ Self_Start_System/Client/src/app/patient-resources/patient-resources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-resources',
+  templateUrl: './patient-resources.component.html',
+  styleUrls: ['./patient-resources.component.scss']
+})
+export class PatientResourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
